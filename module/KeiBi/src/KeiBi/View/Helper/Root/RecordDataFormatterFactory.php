0d8a2cb5eec4d5a9f6e1488d5db8b7a79095a764
jriedl <johannes.riedl@uni-tuebingen.de>
2023-05-23 16:38:20 +0200

    Missing file

---
+++ module/KeiBi/src/KeiBi/View/Helper/Root/RecordDataFormatterFactory.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace KeiBi\View\Helper\Root;
+
+use Interop\Container\ContainerInterface;
+use VuFind\View\Helper\Root\RecordDataFormatter\SpecBuilder;
+
+class RecordDataFormatterFactory extends \IxTheo\View\Helper\Root\RecordDataFormatterFactory {
+
+    /**
+     * User Account Capabilites Service
+     * @var \VuFind\Config\AccountCapabilities
+     */
+    protected $accountCapabilities;
+
+    /**
+     * Db Table Plugin Manager (e.g. to check user-specific rights)
+     * @var \VuFind\Db\Table\PluginManager
+     */
+    protected $dbTablePluginManager;
+
+    /**
+     * The logged in user, or null if not logged in
+     * @var \VuFind\Db\Row\User
+     */
+    protected $user;
+
+    protected $tuefind;
+
+    public function __invoke(ContainerInterface $container, $requestedName,
+        array $options = null
+    ) {
+        $this->user = $container->get('ViewHelperManager')->get('auth')->getManager()->isLoggedIn();
+        $this->tuefind = $container->get('ViewHelperManager')->get('tuefind');
+        $this->dbTablePluginManager = $container->get('VuFind\Db\Table\PluginManager');
+        $this->accountCapabilities = $container->get(\VuFind\Config\AccountCapabilities::class);
+        return parent::__invoke($container, $requestedName, $options);
+    }
+
+    /**
+     * Get default specifications for displaying data in core metadata.
+     *
+     * @return array
+     */
+    public function getDefaultCoreSpecs()
+    {
+        $spec = new SpecBuilder();
+        $this->addPublishedIn($spec);
+        $this->addFollowingTitle($spec); // TueFind specific
+        $this->addPrecedingTitle($spec);  // TueFind specific
+        // Other Titles (IxTheo-specific)
+        $spec->setLine(
+            'Other Titles', 'getOtherTitles'
+        );
+        $this->addDeduplicatedAuthors($spec);
+        $this->addFormats($spec);
+        $this->addLanguages($spec);
+        $this->addSubito($spec);
+        $this->addHBZ($spec);
+        $this->addJOP($spec);
+        // PDA (IxTheo-specific)
+        if ($this->accountCapabilities->getPdaSetting()) {
+            $spec->setTemplateLine(
+                'PDA', 'showPDA', 'data-PDA.phtml', ['rowId' => 'pda_row']
+            );
+        }
+        // TAD (IxTheo-specific)
+        if ($this->user != null && $this->dbTablePluginManager->get('user')->canUseTAD($this->user->id)) {
+            $spec->setTemplateLine(
+                'TAD', 'workIsTADCandidate', 'data-TAD.phtml'
+            );
+        }
+        $this->addPublications($spec);
+        $this->addContainerIdsAndTitles($spec);
+
+        $spec->setTemplateLine('all_reviews', 'getKeibiReviews', 'data-keibiReviews.phtml');
+
+        $this->addVolumesAndArticles($spec);
+        $this->addEdition($spec);
+        $this->addSeries($spec);
+        // Standardized Subjects (IxTheo-specific)
+        $spec->setTemplateLine(
+            'Standardized Subjects', 'getAllStandardizedSubjectHeadings', 'data-allStandardizedSubjectHeadings.phtml'
+        );
+
+        // Non-standardized Subjects (IxTheo-specific)
+        $spec->setTemplateLine(
+            'Nonstandardized Subjects', 'getAllNonStandardizedSubjectHeadings', 'data-allNonStandardizedSubjectHeadings.phtml'
+        );
+        $this->addChildRecords($spec);
+        $this->addOnlineAccess($spec);
+        $this->addLicense($spec); // TueFind specific
+        $this->addRecordLinks($spec);
+        $this->addTags($spec);
+
+        return $spec->getArray();
+    }
+
+    public function getDefaultDescriptionSpecs()
+    {
+        $spec = new SpecBuilder();
+        $spec->setTemplateLine('Summary', true, 'data-summary.phtml');
+        $spec->setLine('Published', 'getDateSpan');
+        // Item Description (IxTheo-specific)
+        $spec->setTemplateLine('Item Description', 'getGeneralNotes', 'data-general-notes.phtml');
+        $spec->setLine('Physical Description', 'getPhysicalDescriptions');
+        $spec->setLine('Publication Frequency', 'getPublicationFrequency');
+        $spec->setLine('Playing Time', 'getPlayingTimes');
+        $spec->setLine('Format', 'getSystemDetails');
+        $spec->setLine('Audience', 'getTargetAudienceNotes');
+        $spec->setLine('Awards', 'getAwards');
+        $spec->setLine('Production Credits', 'getProductionCredits');
+        $spec->setLine('Bibliography', 'getBibliographyNotes');
+        // Clean ISBN with schema.org-property (IxTheo-specific)
+        $spec->setLine(
+            'ISBN', 'getCleanISBN', null,
+            ['prefix' => '<span property="isbn">', 'suffix' => '</span>']
+        );
+        // ISSN with schema.org-property (IxTheo-specific)
+        $spec->setLine(
+            'ISSN', 'getISSNs', null,
+            ['prefix' => '<span property="issn">', 'suffix' => '</span>']
+        );
+        $spec->setLine('Related Items', 'getRelationshipNotes');
+        $spec->setLine('Access', 'getAccessRestrictions');
+        $spec->setLine('Finding Aid', 'getFindingAids');
+        $spec->setLine('Publication_Place', 'getHierarchicalPlaceNames');
+        $spec->setTemplateLine('Author Notes', true, 'data-authorNotes.phtml');
+        return $spec->getArray();
+    }
+}
